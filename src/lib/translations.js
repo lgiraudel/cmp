fe3e37b089611c995bac00656ecf8b9ad3334b2c
Loïc Giraudel <lgiraudel@purch.com>
2018-06-11 11:05:32 +0200

    Fix translations for non-UTF8 sites

--- src/lib/translations.js
+++ src/lib/translations.js
@@ -28,33 +28,33 @@ export default {
 				description: ''
 			},
 			purpose1: {
-				description: 'Allow storing or accessing information on a user’s device.'
+				description: 'Allow storing or accessing information on a user&apos;s device.'
 			},
 			purpose2: {
-				description: `Allow processing of a user’s data to provide and inform personalised advertising (including delivery, measurement, and reporting) based on a user’s preferences or interests known or inferred from data collected across multiple sites, apps, or devices; and/or accessing or storing information on devices  for that purpose.
+				description: `Allow processing of a user&apos;s data to provide and inform personalised advertising (including delivery, measurement, and reporting) based on a user&apos;s preferences or interests known or inferred from data collected across multiple sites, apps, or devices; and/or accessing or storing information on devices  for that purpose.
 				Will include following Features:
 				<ul>
 					<li>Matching Data to Offline Sources - combining data from offline sources that were initially collected in other contexts.</li>
-					<li>Linking Devices - allow processing of a user’s data to connect such user across multiple devices.</li>
-					<li>Precise Geographic Location data - allow processing of a user’s precise geographic location data in support of a purpose for which that certain third party has consent.</li>
+					<li>Linking Devices - allow processing of a user&apos;s data to connect such user across multiple devices.</li>
+					<li>Precise Geographic Location data - allow processing of a user&apos;s precise geographic location data in support of a purpose for which that certain third party has consent.</li>
 				</ul>`
 			},
 			purpose3: {
-				description: `Allow processing of a user’s data to deliver content or advertisements and measure the delivery of such content or advertisements, extract insights and generate reports to understand service usage; and/or accessing or storing information on devices for that purpose.  
+				description: `Allow processing of a user&apos;s data to deliver content or advertisements and measure the delivery of such content or advertisements, extract insights and generate reports to understand service usage; and/or accessing or storing information on devices for that purpose.  
 				Will include following Features:
 				<ul>
 					<li>Matching Data to Offline Sources - combining data from offline sources that were initially collected in other contexts.</li>
-					<li>Linking Devices - allow processing of a user’s data to connect such user across multiple devices.</li>
-					<li>Precise Geographic Location data - allow processing of a user’s precise geographic location data in support of a purpose for which that certain third party has consent.</li>
+					<li>Linking Devices - allow processing of a user&apos;s data to connect such user across multiple devices.</li>
+					<li>Precise Geographic Location data - allow processing of a user&apos;s precise geographic location data in support of a purpose for which that certain third party has consent.</li>
 				</ul>`
 			},
 			purpose4: {
-				description: `Allow processing of a user’s data to provide and inform personalised content (including delivery, measurement, and reporting) based on a user’s preferences or interests known or inferred from data collected across multiple sites, apps, or devices; and/or accessing or storing information on devices for that purpose.  
+				description: `Allow processing of a user&apos;s data to provide and inform personalised content (including delivery, measurement, and reporting) based on a user&apos;s preferences or interests known or inferred from data collected across multiple sites, apps, or devices; and/or accessing or storing information on devices for that purpose.  
 				Will include following Features:
 				<ul>
 					<li>Matching Data to Offline Sources - combining data from offline sources that were initially collected in other contexts.</li>
-					<li>Linking Devices - allow processing of a user’s data to connect such user across multiple devices.</li>
-					<li>Precise Geographic Location data - allow processing of a user’s precise geographic location data in support of a purpose for which that certain third party has consent.</li>
+					<li>Linking Devices - allow processing of a user&apos;s data to connect such user across multiple devices.</li>
+					<li>Precise Geographic Location data - allow processing of a user&apos;s precise geographic location data in support of a purpose for which that certain third party has consent.</li>
 				</ul>`
 			},
 			customPurpose1: {
@@ -137,53 +137,53 @@ export default {
 	fr: {
 		intro: {
 			title: 'Merci de votre visite sur ',
-			description: 'Afin de financer la création de contenu de qualité, nous et certains services tiers déposons des cookies et stockons ou accédons à des données sur votre appareil pour de multiples raisons. Certains services tiers collectent également des données afin de vous montrer un contenu et des publicités personnalisés. Certains de ces services requièrent votre consentement pour collecter et utiliser vos données.',
+			description: 'Afin de financer la cr&eacute;ation de contenu de qualit&eacute;, nous et certains services tiers d&eacute;posons des cookies et stockons ou acc&eacute;dons &agrave; des donn&eacute;es sur votre appareil pour de multiples raisons. Certains services tiers collectent &eacute;galement des donn&eacute;es afin de vous montrer un contenu et des publicit&eacute;s personnalis&eacute;s. Certains de ces services requi&egrave;rent votre consentement pour collecter et utiliser vos donn&eacute;es.',
 			acceptAll: 'J\'accepte',
 			rejectAll: 'Je refuse',
-			showPurposes: 'Voir les détails'
+			showPurposes: 'Voir les d&eacute;tails'
 		},
 		details: {
-			title: 'Préférences utilisateur',
+			title: 'Pr&eacute;f&eacute;rences utilisateur',
 			back: 'Retour',
 			save: 'Sauver et quitter'
 		},
 		purposes: {
-			active: 'Activé',
-			inactive: 'Désactivé',
+			active: 'Activ&eacute;',
+			inactive: 'D&eacute;sactiv&eacute;',
 			showVendors: 'Voir la liste des partenaires',
 			cookies: {
 				menu: 'Utilisation des cookies',
 				title: 'Ce site utilise des cookies',
-				description: 'Nos partenaires et nous établissons des cookies (petits fichiers texte) et recueillons des informations pendant votre navigation sur le Web dans ce navigateur. Ces informations servent à comprendre vos centres d\'intérêt et vous proposer un contenu et des publicités pertinents.'
+				description: 'Nos partenaires et nous &eacute;tablissons des cookies (petits fichiers texte) et recueillons des informations pendant votre navigation sur le Web dans ce navigateur. Ces informations servent &agrave; comprendre vos centres d\'int&eacute;r&ecirc;t et vous proposer un contenu et des publicit&eacute;s pertinents.'
 			},
 			purpose1: {
-				menu: 'Stockage d\'informations et accès',
-				title: 'Stockage d\'informations et accès',
-				description: 'Autoriser le stockage d’informations ou l’accès à des informations déjà stockées sur votre appareil, telles que des identifiants publicitaires, des identifiants de dispositif, des cookies et des technologies similaires.'
+				menu: 'Stockage d\'informations et acc&egrave;s',
+				title: 'Stockage d\'informations et acc&egrave;s',
+				description: 'Autoriser le stockage d&apos;informations ou l&apos;acc&egrave;s &agrave; des informations d&eacute;j&agrave; stock&eacute;es sur votre appareil, telles que des identifiants publicitaires, des identifiants de dispositif, des cookies et des technologies similaires.'
 			},
 			purpose2: {
 				menu: 'Personnalisation',
 				title: 'Personnalisation',
-				description: 'Autoriser la collecte et le traitement d’informations sur votre utilisation de ce service pour ensuite personnaliser la publicité et/ou le contenu qui vous sont proposés dans d’autres contextes, tels que sur d’autres sites ou applications, au fil du temps. En règle générale, le contenu du site ou de l’application est utilisé pour déterminer vos centres d’intérêt et permettent de déterminer le choix de la publicité et/ou du contenu.'
+				description: 'Autoriser la collecte et le traitement d&apos;informations sur votre utilisation de ce service pour ensuite personnaliser la publicit&eacute; et/ou le contenu qui vous sont propos&eacute;s dans d&apos;autres contextes, tels que sur d&apos;autres sites ou applications, au fil du temps. En r&egrave;gle g&eacute;n&eacute;rale, le contenu du site ou de l&apos;application est utilis&eacute; pour d&eacute;terminer vos centres d&apos;int&eacute;r&ecirc;t et permettent de d&eacute;terminer le choix de la publicit&eacute; et/ou du contenu.'
 			},
 			purpose3: {
-				menu: 'Sélection d\'annonces, diffusion, rapport',
-				title: 'Sélection d\'annonces, diffusion, rapport',
-				description: 'Autoriser le traitement des données d\'un utilisateur pour fournir du contenu ou des publicités et mesurer la diffusion de ces contenus ou publicités, extraire des informations et générer des rapports pour comprendre l\'utilisation des services; et / ou accéder ou stocker des informations sur des dispositifs à cette fin.'
+				menu: 'S&eacute;lection d\'annonces, diffusion, rapport',
+				title: 'S&eacute;lection d\'annonces, diffusion, rapport',
+				description: 'Autoriser le traitement des donn&eacute;es d\'un utilisateur pour fournir du contenu ou des publicit&eacute;s et mesurer la diffusion de ces contenus ou publicit&eacute;s, extraire des informations et g&eacute;n&eacute;rer des rapports pour comprendre l\'utilisation des services; et / ou acc&eacute;der ou stocker des informations sur des dispositifs &agrave; cette fin.'
 			},
 			purpose4: {
-				menu: 'Sélection de contenu, diffusion, rapport',
-				title: 'Sélection de contenu, diffusion, rapport',
-				description: 'Autoriser le traitement des données d\'un utilisateur pour la création de contenu personnalisé (y compris la diffusion, l\'analyse et la création de rapports) en fonction des préférences ou des intérêts d\'un utilisateur connus ou inférer à partir de données collectées sur plusieurs sites, applications ou appareils; et / ou accéder ou stocker des informations sur des dispositifs à cette fin.'
+				menu: 'S&eacute;lection de contenu, diffusion, rapport',
+				title: 'S&eacute;lection de contenu, diffusion, rapport',
+				description: 'Autoriser le traitement des donn&eacute;es d\'un utilisateur pour la cr&eacute;ation de contenu personnalis&eacute; (y compris la diffusion, l\'analyse et la cr&eacute;ation de rapports) en fonction des pr&eacute;f&eacute;rences ou des int&eacute;r&ecirc;ts d\'un utilisateur connus ou inf&eacute;rer &agrave; partir de donn&eacute;es collect&eacute;es sur plusieurs sites, applications ou appareils; et / ou acc&eacute;der ou stocker des informations sur des dispositifs &agrave; cette fin.'
 			},
 			purpose5: {
 				menu: 'Mesures',
 				title: 'Mesures',
-				description: 'Autoriser la collecte d’informations sur votre utilisation du contenu et la combinaison avec des informations précédemment collectées, utilisées pour mesurer, comprendre et rendre compte de votre utilisation du service. Cela n’inclut pas la personnalisation, la collecte d’informations sur votre utilisation de ce service pour personnaliser ultérieurement le contenu et/ou la publicité dans d’autres contextes, par exemple sur d’autres services, tels que des sites ou des applications.'
+				description: 'Autoriser la collecte d&apos;informations sur votre utilisation du contenu et la combinaison avec des informations pr&eacute;c&eacute;demment collect&eacute;es, utilis&eacute;es pour mesurer, comprendre et rendre compte de votre utilisation du service. Cela n&apos;inclut pas la personnalisation, la collecte d&apos;informations sur votre utilisation de ce service pour personnaliser ult&eacute;rieurement le contenu et/ou la publicit&eacute; dans d&apos;autres contextes, par exemple sur d&apos;autres services, tels que des sites ou des applications.'
 			},
 			customPurpose1: {
-				menu: 'Publicités personnalisées par Google',
-				title: 'Publicités personnalisées par Google',
+				menu: 'Publicit&eacute;s personnalis&eacute;es par Google',
+				title: 'Publicit&eacute;s personnalis&eacute;es par Google',
 				description: ''
 			}
 		},
@@ -191,9 +191,9 @@ export default {
 			title: 'Nos partenaires',
 			rejectAll: 'Tout refuser',
 			acceptAll: 'Tout accepter',
-			company: 'Société',
+			company: 'Soci&eacute;t&eacute;',
 			offOn: 'Autoriser',
-			description: 'Aidez-nous à vous fournir une meilleur expérience utilisateur ! Nos partenaires déposent des cookies et collectent des informations depuis votre navigateur pour vous fournir un meilleur contenu, des publicités pertinentes ainsi que pour analyser l\'audience globale du site',
+			description: 'Aidez-nous &agrave; vous fournir une meilleur exp&eacute;rience utilisateur ! Nos partenaires d&eacute;posent des cookies et collectent des informations depuis votre navigateur pour vous fournir un meilleur contenu, des publicit&eacute;s pertinentes ainsi que pour analyser l\'audience globale du site',
 			moreChoices: 'Choisir les partenaires'
 		},
 		footer: {
